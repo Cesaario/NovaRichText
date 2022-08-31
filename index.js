f40555d21d7703e51f0fe15e60f28206972f58aa
Gustavo <gs.cesario1998@gmail.com>
2022-08-30 23:40:19 -0300

    correcoes

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var RichText = (id) => {
   const container = document.getElementById(id);
 
   const input = document.createElement("div");
-  input.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  input.innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
   const editor = criarEditor();
 
   input.setAttribute("contenteditable", "true");
@@ -16,14 +17,15 @@ var RichText = (id) => {
   container.style.position = "relative";
 
   function onMouseUp() {
-
     const { x, y } = window
       .getSelection()
       .getRangeAt(0)
       .getBoundingClientRect();
 
-    editor.style.top = `${parseInt(y + 20)}px`;
-    editor.style.left = `${parseInt(x)}px`;
+    console.log(editor.offsetTop);
+
+    editor.style.top = `${parseInt(y + 20 - container.offsetTop)}px`;
+    editor.style.left = `${parseInt(x - container.offsetLeft)}px`;
 
     editor.hidden = window.getSelection().toString().length === 0;
 
@@ -31,19 +33,18 @@ var RichText = (id) => {
   }
 
   function criarEditor() {
+    function criarBotao(nomeIcone, comando) {
+      const botao = document.createElement("button");
+      const icone = document.createElement("i");
+      icone.classList.add("fa-solid");
+      icone.classList.add(nomeIcone);
 
-    function criarBotao(nomeIcone, comando){
-        const botao = document.createElement("button");
-        const icone = document.createElement("i");
-        icone.classList.add("fa-solid");
-        icone.classList.add(nomeIcone);
-
-        botao.classList.add("botao");
-        botao.appendChild(icone);
-        botao.onclick = () => {
-            document.execCommand(comando);
-        }
-        return botao;
+      botao.classList.add("botao");
+      botao.appendChild(icone);
+      botao.onclick = () => {
+        document.execCommand(comando);
+      };
+      return botao;
     }
 
     const editor = document.createElement("div");
