30abec34caf9aa752c3a1ce86e0a3b3cf387e5b8
Gustavo <gs.cesario1998@gmail.com>
2022-08-31 21:38:40 -0300

    progresso hein

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var RichText = (id, handleChange, valorDefault) => {
     editor.style.top = `${parseInt(y + 20 - yOffset)}px`;
     editor.style.left = `${parseInt(x - xOffset)}px`;
 
-    editor.hidden = window.getSelection().toString().length === 0;
+    editor.style.display = window.getSelection().toString().length === 0 ? "none" : "flex";
   }
 
   function criarEditor() {
@@ -45,15 +45,74 @@ var RichText = (id, handleChange, valorDefault) => {
       return botao;
     }
 
+    function criarDivider(){
+      const divider = document.createElement("div");
+      divider.classList.add("divider");
+      
+      return divider;
+    }
+
+    function criarSeletorTamanho(){
+      const seletorTamanho = document.createElement("div");
+      const botao = document.createElement("button");
+      botao.classList.add("botao");
+      botao.classList.add("botaoSeletorTamanho");
+
+      const iconeFonte = document.createElement("i");
+      iconeFonte.classList.add("fa-solid");
+      iconeFonte.classList.add("fa-font");
+
+      const iconeChevron = document.createElement("i");
+      iconeChevron.classList.add("fa-solid");
+      iconeChevron.classList.add("fa-chevron-down");
+      iconeChevron.style.fontSize = "8px"
+      iconeChevron.style.marginLeft = "2px"
+
+      const menuOpcoes = document.createElement("div");
+      menuOpcoes.classList.add("menuOpcoesTamanho");
+
+      botao.appendChild(iconeFonte);
+      botao.appendChild(iconeChevron);
+      botao.addEventListener("click", () => menuOpcoes.style.display = menuOpcoes.style.display === "none" ? "flex" : "none")
+
+      seletorTamanho.appendChild(botao);
+      seletorTamanho.appendChild(menuOpcoes);
+
+      return seletorTamanho;
+    }
+
+    function criarSeletorTamanhofgf() {
+
+      function criarOpcao(descricao, comando){
+        const opcao = document.createElement("option");
+        opcao.setAttribute("value", descricao);
+        opcao.innerHTML = descricao;
+        return opcao;
+      }
+
+      const seletor = document.createElement("select");
+      seletor.setAttribute("name", "tamanho");
+      seletor.classList.add("seletorTamanho");
+
+      seletor.appendChild(criarOpcao("H1", "asdasda"));
+      seletor.appendChild(criarOpcao("H2", "asdasda"));
+
+      return seletor;
+    }
+
     const editor = document.createElement("div");
     editor.classList.add("editor");
 
     editor.hidden = true;
 
+    editor.appendChild(criarSeletorTamanho());
     editor.appendChild(criarBotao("fa-bold", "bold"));
     editor.appendChild(criarBotao("fa-italic", "italic"));
     editor.appendChild(criarBotao("fa-underline", "underline"));
     editor.appendChild(criarBotao("fa-strikethrough", "strikethrough"));
+    editor.appendChild(criarDivider());
+    editor.appendChild(criarBotao("fa-list-ol", "insertOrderedList"));
+    editor.appendChild(criarBotao("fa-list", "insertUnorderedList"));
 
     return editor;
   }
